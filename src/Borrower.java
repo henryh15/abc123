a9aff862ccc35834c7cf2e0557de6446122a42a3
Johnny <jchien922@gmail.com>
2012-11-21 17:29:28 -0800

    Basic structure of the project created

--- src/Borrower.java
+++ src/Borrower.java
@@ -1,4 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Borrower {
-
+	
+	private BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	private DBConnect db;
+	private Connection con;
+	
+	public Borrower (DBConnect connect){
+		db = connect;
+		con = db.getCon();
+	}
 }
